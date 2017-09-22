738f0adb08caa3fc3ef211e79257c415ae56cf17
Pablo Sanches <sanches.webmaster@gmail.com>
2017-09-22 09:07:59 -0300

    Skeleton project

---
+++ src/Canary/Mailer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Canary;
+
+/**
+ * Class Mailer
+ *
+ * @author Pablo Sanches <sanches.webmaster@gmail.com>
+ * @license MIT
+ */
+class Mailer
+{
+
+}
---
+++ src/Canary/MailerStack.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Canary;
+
+use \SplObjectStorage;
+
+/**
+ * Class MailerStack
+ *
+ * @author Pablo Sanches <sanches.webmaster@gmail.com>
+ * @license MIT
+ */
+class MailerStack extends SplObjectStorage
+{
+
+}
